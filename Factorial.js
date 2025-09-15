4be1d1df1fefad49a8450d674c4d7ad52da5245a
Plauuss <deluxerelax4@gmail.com>
2025-09-15 11:24:39 +0800

    added readline interface

--- Factorial.js
+++ Factorial.js
@@ -1,8 +1,5 @@
 console.log("factorial application");
 
-  console.log("2.compute factorial");
-   console.log("3.exit");
-
    const readline = require("readline");
 const rl = readline.createInterface({
     input: process.stdin,
@@ -16,33 +13,6 @@ const rl = readline.createInterface({
 
 
 console.log("1. say hello");
-rl.question("enter your choice(1-3)", choice => {
-if (choice === "1"){
-console.log("hello there");
 
-   } else if (choice === "2"){
-    console.log("factorial");
-
-    rl.question("enter a number for factorial: ", (numstr) =>{
-let num = parseInt(numstr);
-
-if(isNaN(num) || num<0){
-  console.log("Please enter a non-negative integer");
-} else {
-  let fact = 1;
-
-  for(let i= 1; i <= num ; i++){
-    fact *=i;
-  }
-  console.log ("The factorial of " + num +" is " + " " +fact);
-}
-    });
-   
-   }else if (choice === "3"){
-    console.log("exit");
-   }else {
-    console.log("invalid choice. please try again");
-
-   }
-   }
-   )
+  console.log("2.compute factorial");
+   console.log("3.exit");
